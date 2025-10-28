72f6c120ecaef9fd81d5d850b4749885634167c3
yazan <yazanmohamed16@gmail.com>
2025-10-28 15:20:53 +0200

    Add initial implementation of data types in PHP

---
+++ data Types/dataTypes.php
@@ -0,0 +1,18 @@
+<?php
+//Date type in php language 
+//- Primitive Types
+$name="yazan"; //String 
+
+$age=22; //Integer (Numeric Values)
+
+$gpa=3.1; //Double or Float (Decimal VAlues)
+
+$is_graduated=false; //Boolean
+
+//and there is a null type like :
+$value=null;//nothing to print.
+
+echo "Hello my name is $name, I am $age years old, my GPA is $gpa, and I have  ".($is_graduated ?"graduated.":"not graduated yet.")."<br>";
+
+
+//There is another type like arrays will show it later.
